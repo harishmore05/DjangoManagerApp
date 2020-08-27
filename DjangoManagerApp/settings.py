b3c8424e80504a3d3dd4001e616973f10a5f0ae1
Harish_More <harishmore05@gmail.com>
2020-08-27 12:30:28 +0530

    Before Readme Update

--- DjangoManagerApp/settings.py
+++ DjangoManagerApp/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'DjangoManagerApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django3',
+        'USER': 'django2',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT':'',
     }
 }
 
--- api/user/models.py
+++ api/user/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+# from django.utils.translation import ugettext_lazy as _
 
 # User model
 class CustomUser(AbstractUser):
@@ -15,9 +17,51 @@ class CustomUser(AbstractUser):
     username = None
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    is_subscribed = models.BooleanField(default=False)
     
     session_token = models.CharField(max_length=10, default='0')
     
     create_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_subscribed = models.BooleanField(default=False)
+
+# User Manger Model
+class UserManager(BaseUserManager):
+    def create_user(self, email, full_name, profile_picture, password=None, is_admin=False, is_staff=False, is_active=True):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+        if not full_name:
+            raise ValueError("User must have a full name")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.full_name = full_name
+        user.set_password(password)  # change password to hash
+        user.profile_picture = profile_picture
+        user.admin = is_admin
+        user.staff = is_staff
+        user.active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, profile_picture, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+        if not full_name:
+            raise ValueError("User must have a full name")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.full_name = full_name
+        user.set_password(password)
+        user.profile_picture = profile_picture
+        user.admin = True
+        user.staff = True
+        user.active = is_active
+        user.save(using=self._db)
+        return user
